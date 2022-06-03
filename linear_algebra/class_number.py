dd035ea84f7725e2edcc9e194b4fa1f0cac5b143
ChoHyunGook <whgusrnr7755@gmail.com>
2022-06-03 17:45:52 +0900

    vector n matrix data record

--- linear_algebra/class_number.py
+++ linear_algebra/class_number.py
@@ -9,8 +9,7 @@ class class_number:
         self.samples = [0, 10, 20, 30, 1, 11, 21, 31]
         self.d=[]
         [self.d.append(self.digits.images[self.samples[i]]) for i in range(8)]
-        self.v=[]
-        [self.v.append(self.d[i].reshape(64, 1)) for i in range(8)]
+
 
     def data_load(self):
         plt.figure(figsize=(8, 2))
@@ -23,13 +22,18 @@ class class_number:
             plt.title("image {}".format(i + 1))
         plt.suptitle("숫자 0과 1 이미지")
         plt.tight_layout()
-        plt.show()
+        #plt.show()
+        plt.savefig('test_matrix.png',
+                    facecolor='#eeeeee',
+                    edgecolor='black',
+                    format='png', dpi=200)
 
     def vector_image(self):
+        v = [(self.d[i].reshape(64, 1)) for i in range(8)]
         plt.figure(figsize=(8, 3))
         for i in range(8):
             plt.subplot(1, 8, i + 1)
-            plt.imshow(self.v[i], aspect=0.4,
+            plt.imshow(v[i], aspect=0.4,
                        interpolation='nearest', cmap=plt.cm.bone_r)
             plt.grid(False)
             plt.xticks([])
@@ -37,7 +41,11 @@ class class_number:
             plt.title("벡터 {}".format(i + 1))
         plt.suptitle("벡터화된 이미지", y=1.05)
         plt.tight_layout(w_pad=7)
-        plt.show()
+        #plt.show()
+        plt.savefig('test_vector.png',
+                    facecolor='#eeeeee',
+                    edgecolor='black',
+                    format='png', dpi=200)
 
 
 
